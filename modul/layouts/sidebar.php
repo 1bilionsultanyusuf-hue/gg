f46215dcddcd62862275313b3c281a13df17b6df
Xeno <1bilionsultanyusuf@gmail.com>
2025-08-15 13:05:17 +0700

    Update sidebar.php

--- modul/layouts/sidebar.php
+++ modul/layouts/sidebar.php
@@ -1,5 +1,5 @@
 <div class="sidebar bg-white w-64 h-full fixed shadow-lg z-10">
-    <div class="flex items-center p-3 border-b">
+    <div class="flex items-center p-5 border-b">
         <span class="logo-text ml-3 font-bold text-xl text-gray-800">MENU</span>
         <button id="toggleSidebar" class="text-gray-500 hover:text-gray-700 menu-button">
             <i class="fas fa-bars"></i>
