ca99f858f08eaab1ba989a4d5f6016c2cf2f0935
Xeno <1bilionsultanyusuf-hue@users.noreply.github.com>
2025-09-01 13:48:47 +0700

    update

--- modul/layouts/footer.php
+++ modul/layouts/footer.php
@@ -7,10 +7,5 @@
         <div class="footer-text">
             &copy; <?= date('Y') ?> IT | CORE - Creative Prototype System
         </div>
-        <div class="footer-meta">
-            <span class="footer-version">v1.0.0</span>
-            <span class="footer-separator">•</span>
-            <span class="footer-author">Developed by pr</span>
-        </div>
     </div>
 </footer>
